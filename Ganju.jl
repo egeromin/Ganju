c84d9735b14e794939db6b70cb01c7465a3f8295
Emanuel Geromin <egeromin@gmail.com>
2019-03-17 14:44:22 +0000

    Refactor model definition

--- Ganju.jl
+++ Ganju.jl
@@ -1,25 +1,33 @@
 module Ganju
 
-using Flux  # julia ML library
+using Flux, FLUX.data.MNIST  # julia ML library
 
+imgs = MNIST.images()
 
 # target: generate digits using MNIST data
 
-# TODO: 
+# TODO:
 # 1. define a generator net
 # 2. define a discriminator net
 # 3. define the cost function and then perform a gradient descent update
+INPUT_DIM_DIS = 784
+HIDDEN_DIM = 64
+OUTPUT_DIM_DIS = 1
+INPUT_DIM_GEN = 64
+OUTPUT_DIM_GEN = INPUT_DIM_DIS
+
 
 
 function make_discriminator()
-    Chain(Dense(28^2, 32, relu),
-          Dense(32, 10),
-          softmax) 
+    Chain(Dense(INPUT_DIM_DIS, HIDDEN_DIM, relu),
+          Dense(HIDDEN_DIM, OUTPUT_DIM_DIS),
+          sigmoid)
 end
 
 function make_generator()
-    Chain(Dense(10, 32, relu),
-          Dense(32, 28^2), sigmoid)
+    Chain(Dense(INPUT_DIM_GEN, HIDDEN_DIM, relu),
+          Dense(HIDDEN_DIM, OUTPUT_DIM_GEN),
+          sigmoid)
 end
 
 function fetch_training_sample()
